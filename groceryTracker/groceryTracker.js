afcea476a1507b2ff82ea96324017b88724eb583
nathantorento <nathanjohntorento@gmail.com>
2025-11-14 12:43:46 -0500

    message

---
+++ groceryTracker/groceryTracker.js
@@ -0,0 +1,12 @@
+let grocery1;
+let grocery2;
+let grocery3;
+
+function calculateTotal() {
+    grocery1 = parseFloat(document.getElementById("grocery1").value);
+    grocery2 = parseFloat(document.getElementById("grocery2").value);
+    grocery3 = parseFloat(document.getElementById("grocery3").value);
+
+    totalAmount = grocery1 + grocery2 + grocery3
+    document.getElementById("result2").innerText = `The total amount is: $${totalAmount}`;
+}
