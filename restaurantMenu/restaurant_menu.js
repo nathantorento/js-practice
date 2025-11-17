6c9e27fa6bfbcd6c475ba1b25dc319ab76176fb5
nathantorento <nathanjohntorento@gmail.com>
2025-11-16 21:17:39 -0800

    did restaurant_menu challenge

--- restaurantMenu/restaurant_menu.js
+++ restaurantMenu/restaurant_menu.js
@@ -1,8 +1,8 @@
-const breakfastMenu = ['Pancakes', 'Eggs Benedict', 'Oatmeal', 'Frittata'];
-const mainCourseMenu = ['Steak', 'Pasta', 'Burger', 'Salmon'];
-const dessertMenu = ['Cake', 'Ice Cream', 'Pudding', 'Fruit Salad'];
+const breakfastMenu = ['Pancakes – $12', 'Eggs Benedict – $22.99', 'Oatmeal – $21.99', 'Frittata – $15'];
+const mainCourseMenu = ['Steak – $24.99', 'Pasta – $13', 'Burger – $21', 'Salmon – $22'];
+const dessertMenu = ['Cake – $15', 'Ice Cream – $12', 'Pudding – $9', 'Fruit Salad – $12'];
 
- const breakfastMenuItemsHTML = breakfastMenu.map((item, index) => `<p>Item ${index + 1}: ${item}</p>`).join('');
+ const breakfastMenuItemsHTML = breakfastMenu.map((item, index) => `<p>Item ${index + 1}: ${item} </p>`).join('');
     
  document.getElementById('breakfastMenuItems').innerHTML = breakfastMenuItemsHTML;
 
